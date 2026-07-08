4958ce81272ae974a0e4dde4aee4b891433bf0b6
sinsin13 <adsnh13@gmail.com>
2026-07-08 23:59:14 +0530

    add Acra logo to login page and enhance layout

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,16 +1,29 @@
 import loginBg from "./login-bg.png";
+import acraLogo from "./acra-logo.png";
 
-function login(){
-    return <div className="min-h-screen relative overflow-hidden">
+
+function Login() {
+    return (
+      <div className="min-h-screen relative">
         <div
-        className="absolute -inset-4 bg-cover bg-center"
-        style={{
-          backgroundImage: `url(${loginBg.src})`,
-          filter: "blur(1.5px)",
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${loginBg.src})`,
+            filter: "blur(1.5px)",
         }}
-      />
-    </div>
-}
-
-
-export default login;
+        />
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(0,0,0,0.8), rgba(0,0,0,0.4), rgba(0,0,0,0.8))",
+          }}
+        />
+        <div className="absolute top-2 left-2 z-10">
+          <img src={acraLogo.src} alt="Acra logo" className="h-8 w-auto" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default Login;
