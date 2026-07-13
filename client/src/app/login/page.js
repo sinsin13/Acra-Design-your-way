ae85b299f8d1832d2c44830ad8d1f151dae71a4a
sinsin13 <adsnh13@gmail.com>
2026-07-13 23:50:51 +0530

    refactor login page to use external image sources and clean up background styles

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,46 +1,37 @@
-import loginBg from "./login-bg.png";
-import acraLogo from "./acra-logo.png";
-import LoginCard from "../../components/login/login-card";
+import LoginCard from "@/components/login/login-card";
+import Image from "next/image";
 
 function Login() {
-    return (
-      <div className="min-h-screen relative">
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{
-            backgroundImage: `url(${loginBg.src})`,
-            filter: "blur(0px)",
+  return (
+    <div className="min-h-screen relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url(https://static.canva.com/web/images/543d7829999d351b301ced5ed3c1f087.jpg)",
         }}
+      />
+      <div
+        className="absolute inset-0"
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(0,0,0,0.8), rgba(0,0,0,0.4), rgba(0,0,0,0.8))",
+        }}
+      />
+      <div className="absolute top-4 left-4 z-10">
+        <Image
+          src="https://static.canva.com/web/images/856bac30504ecac8dbd38dbee61de1f1.svg"
+          alt="canva"
+          width={90}
+          height={30}
+          priority
         />
-        <div
-          className="absolute inset-0"
-          style={{
-            background:
-              "linear-gradient(180deg, rgba(0,0,0,0.8), rgba(0,0,0,0.8), rgba(0,0,0,0.8))",
-          }}
-        />
-
-        {/* logo */}
-        <div className="absolute top-7 left-7 z-10">
-          <img
-            src={acraLogo.src}
-            alt="Acra"
-            className="h-18 w-auto"
-            style={{
-              filter: "brightness(1.5) drop-shadow(0 4px 6px rgba(0,0,0,0.9))",
-            }}
-          />
-        </div>
-
-
-        {/* login card */}
-        <div className="relative z-10 flex items-center justify-center min-h-screen">
-        <LoginCard />
-        </div>
-
-
       </div>
-    );
-  }
-  
-  export default Login;
+      <div className="relative z-10 flex items-center justify-center min-h-screen">
+        <LoginCard />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
