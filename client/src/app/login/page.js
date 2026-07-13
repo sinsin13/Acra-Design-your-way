5c94e16744549e6012f39b4282ab52efa1160197
sinsin13 <adsnh13@gmail.com>
2026-07-13 23:51:50 +0530

    revert

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,37 +1,46 @@
-import LoginCard from "@/components/login/login-card";
-import Image from "next/image";
+import loginBg from "./login-bg.png";
+import acraLogo from "./acra-logo.png";
+import LoginCard from "../../components/login/login-card";
 
 function Login() {
-  return (
-    <div className="min-h-screen relative">
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage:
-            "url(https://static.canva.com/web/images/543d7829999d351b301ced5ed3c1f087.jpg)",
+    return (
+      <div className="min-h-screen relative">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${loginBg.src})`,
+            filter: "blur(0px)",
         }}
-      />
-      <div
-        className="absolute inset-0"
-        style={{
-          background:
-            "linear-gradient(180deg, rgba(0,0,0,0.8), rgba(0,0,0,0.4), rgba(0,0,0,0.8))",
-        }}
-      />
-      <div className="absolute top-4 left-4 z-10">
-        <Image
-          src="https://static.canva.com/web/images/856bac30504ecac8dbd38dbee61de1f1.svg"
-          alt="canva"
-          width={90}
-          height={30}
-          priority
         />
-      </div>
-      <div className="relative z-10 flex items-center justify-center min-h-screen">
-        <LoginCard />
-      </div>
-    </div>
-  );
-}
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(0,0,0,0.8), rgba(0,0,0,0.8), rgba(0,0,0,0.8))",
+          }}
+        />
 
-export default Login;
+        {/* logo */}
+        <div className="absolute top-7 left-7 z-10">
+          <img
+            src={acraLogo.src}
+            alt="Acra"
+            className="h-18 w-auto"
+            style={{
+              filter: "brightness(1.5) drop-shadow(0 4px 6px rgba(0,0,0,0.9))",
+            }}
+          />
+        </div>
+
+
+        {/* login card */}
+        <div className="relative z-10 flex items-center justify-center min-h-screen">
+        <LoginCard />
+        </div>
+
+
+      </div>
+    );
+  }
+  
+  export default Login;
